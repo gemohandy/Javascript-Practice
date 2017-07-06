186fa47cc2f5dfb517ceecfbb4ef217d5e3b6c02
Bennett Eidsness <bennetteidsness@gmail.com>
2017-07-06 13:02:42 -0600

    Message Change

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 window.onload = function(){
   document.getElementById('tester').innerHTML="This is a basic test of Javascript";
+  setTimeout(diffMess, 3000);
+};
+
+diffMess = function(){
+  document.getElementById('tester').innerHTML = "I changed the message!";
 };

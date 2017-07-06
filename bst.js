0d24ae586d24b8702c5a40d9f4856f6b61a255b6
Bennett Eidsness <bennetteidsness@gmail.com>
2017-07-06 16:19:53 -0600

    Binary Search Tree Cont.

--- bst.js
+++ bst.js
@@ -4,19 +4,24 @@ sortedArray = unsortArray.sort(function(a,b){return a-b;});
 console.log(sortedArray);
 
 //Tree:
-//33, 
+//33, 4, 2, 1, 3, 23, 16, 77, 45, 42, 56, 100, 99
 
-/*function binaryOrder(sortArray){
+function binaryOrder(sortArray){
   if(sortArray<=1){
     return sortArray;
   }
   newArray = [];
-  midpoint = Math.ceil(sortArray.length/2-1);
-  firstPart = binaryOrder(sortArray.splice(0,midpoint));
-  newArray = newArray.concat(firstPart, binaryOrder(sortArray.splice(1)));
-  console.log(newArray);
-  return newArray;
+  midpoint = Math.floor(sortArray.length/2);
+  firstNum = sortArray[midpoint];
+  console.log(midpoint);
+  firstPart = sortArray.splice(0,midpoint);
+  secondPart =sortArray.splice(1);
+  newArray.push(firstNum);
+  binArray = newArray.concat(binaryOrder(firstPart), binaryOrder(secondPart));
+  console.log(firstNum);
+  console.log(firstPart);
+  console.log(secondPart);
+  return binArray;
 }
 
 console.log(binaryOrder(sortedArray));
-*/
